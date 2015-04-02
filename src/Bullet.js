8802361e9477142822d69039d1e8eefe16eb15d8
Jittat Fakcharoenphol <jittat@gmail.com>
2015-04-03 00:09:37 +0700

    bullets move and hit the ship

---
+++ src/Bullet.js
@@ -0,0 +1,17 @@
+var Bullet = cc.Sprite.extend({
+    ctor: function() {
+        this._super();
+        this.initWithFile( 'res/images/bullet.png' );
+    },
+
+    update: function( dt ) {
+        var x = this.getPositionX();
+        x += 5;
+        this.setPositionX( x );
+    },
+
+    randomPosition: function() {
+        var y = 10 + Math.floor( Math.random() * ( screenHeight - 10 ) );
+        this.setPosition( new cc.Point( 100, y ) );
+    }
+});
--- src/GameLayer.js
+++ src/GameLayer.js
@@ -1,17 +1,60 @@
 var GameLayer = cc.LayerColor.extend({
     init: function() {
-        this._super( new cc.Color( 127, 127, 127, 255 ) );
+        this._super( new cc.Color( 0, 0, 0, 255 ) );
         this.setPosition( new cc.Point( 0, 0 ) );
 
         this.ship = new Ship();
         this.ship.setPosition( new cc.Point( 700, 300 ) );
         this.addChild( this.ship );
 
+        this.bullets = [];
+        for ( var i = 0; i < 10; i++ ) {
+            var bullet = new Bullet();
+            bullet.randomPosition();
+            bullet.setPositionX( 100 - 150 * i );
+            this.addChild( bullet );
+            bullet.scheduleUpdate();
+
+            this.bullets.push( bullet );
+        }
+
+        this.life = 10;
+        this.lifeLabel = cc.LabelTTF.create( 10, 'Arial', 32 );
+        this.lifeLabel.setPosition( cc.p( 700, 550 ) );
+        this.addChild( this.lifeLabel );
+        
         this.addKeyboardHandlers();
+        this.scheduleUpdate();
         
         return true;
     },
 
+    update: function( dt ) {
+        var self = this;
+        this.bullets.forEach( function( bullet, i ) {
+            var x = bullet.getPositionX();
+            if ( ( x < screenWidth ) &&
+                 ( x > screenWidth - 100 ) ) {
+                var y = bullet.getPositionY();
+                if ( Math.abs( y - self.ship.getPositionY() ) < 25 ) {
+                    bullet.randomPosition();
+                    self.life -= 1;
+                    self.lifeLabel.setString( self.life );
+                    if ( self.life == 0 ) {
+                        var gameOverLabel = cc.LabelTTF.create( 'Game over', 'Arial', 60 );
+                        gameOverLabel.setPosition( cc.p( 400, 300 ) );
+                        self.addChild( gameOverLabel );
+                        cc.director.pause();
+                    }
+                    return;
+                }
+            }
+            if ( x > screenWidth ) {
+                bullet.randomPosition();
+            }
+        });
+    },
+    
     onKeyDown: function( keyCode, event ) {
         if ( keyCode == cc.KEY.up ) {
             var y = this.ship.getPositionY();
